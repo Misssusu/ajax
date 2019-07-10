64cf23a709d9b45c0c4db87f2bcbce4c679c5374
suyuan <suyuan326@qq.com>
2019-07-10 15:51:08 +0800

    update

--- main.js
+++ main.js
@@ -1,26 +1,38 @@
-window.jQuery.ajax = function (url, method, body, success, fail) {
-    let request = new XMLHttpRequest()
-    request.open(method, url)
-    request.send(body)
-    request.onreadystatechange = function () {
-        if (request.readyState === 4 && request.status === 200) {
-            success.call(undefined,request)
-        } else if (request.readyState >= 400) {
-            fail.call(undefined,request)
+window.jQuery = function(nodeOrSelector){
+    let nodes = {}
+    nodes.addClass = function(){}
+    nodes.html = function(){}
+    return nodes
+}
+window.jQuery.ajax = function (obj) {
+    let promise = new Promise(function (resolve, reject) {
+        let { url, method, data } = obj;
+        let request = new XMLHttpRequest()
+        request.open(method, url)
+        request.send(data)
+        request.onreadystatechange = function () {
+            if (request.readyState === 4 && request.status === 200) {
+                resolve.call(undefined, request)
+            } else if (request.readyState >= 400) {
+                reject.call(undefined, request)
+            }
         }
-    }
-}
-function success(request) {
-    console.log(request)
-    console.log('请求成功')
-    let string = request.responseText;
-    let obj = JSON.parse(string)
-    let name = obj.name;
-    console.log(name)
-}
-function fail() {
-    console.log('请求失败')
+    })
+    return promise;
 }
 btn.addEventListener('click', function () {
-    jQuery.ajax('/xxx','post','a=1&b=2',success,fail)
+    jQuery.ajax({
+        url: '/xxx',
+        method: 'post',
+        data: 'a=1&b=2'
+    }).then((request) => {
+        console.log(request)
+        console.log('请求成功')
+        let string = request.responseText;
+        let obj = JSON.parse(string)
+        let name = obj.name;
+        console.log(name)
+    }, (request) => {
+        console.log('请求失败')
+    })
 })
